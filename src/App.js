2418184c44515ddae3e95e6e054accc874c99340
suyashrastogi7 <suyashrastogi@protonmail.com>
2022-04-13 02:58:43 +0530

    Added dot navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 
+import { Element, Link } from "react-scroll";
+
+//Images
+import dot from "./Images/dot.svg";
+
 //Components
 import Landing from "./Components/Landing";
 import Intro from "./Components/Intro";
@@ -10,15 +15,51 @@ import Offers from "./Components/Offers";
 import Footer from "./Components/Footer";
 
 function App() {
+  const screens = [
+    "landing",
+    "intro",
+    "menu",
+    "steaks",
+    "bar",
+    "offers",
+    "footer",
+  ];
   return (
     <div className="App">
-      <Landing />
-      <Intro />
-      <Menu />
-      <Steaks />
-      <Bar />
-      <Offers />
-      <Footer />
+      <div className="fixed right-12 flex flex-col z-50 top-1/2 -translate-y-1/2">
+        {screens.map((item, i) => {
+          return (
+            <Link to={item} key={i} spy={true} smooth={true} duration={500}>
+              <img
+                src={dot}
+                alt="dot"
+                className="h-6 w-6 my-2 opacity-75 hover:opacity-100 cursor-pointer"
+              />
+            </Link>
+          );
+        })}
+      </div>
+      <Element name="landing">
+        <Landing />
+      </Element>
+      <Element name="intro">
+        <Intro />
+      </Element>
+      <Element name="menu">
+        <Menu />
+      </Element>
+      <Element name="steaks">
+        <Steaks />
+      </Element>
+      <Element name="bar">
+        <Bar />
+      </Element>
+      <Element name="offers">
+        <Offers />
+      </Element>
+      <Element name="footer">
+        <Footer />
+      </Element>
     </div>
   );
 }
